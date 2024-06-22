39ab938a59912043842596abc4ade8a241bce2c5
lulunkaii <sofyiaolate@gmail.com>
2024-06-22 16:25:51 -0400

    feat: se agregan archivos de utilidades para carga de recursos y operaciones de querys

---
+++ queryFunctions.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+def connect_to_database():
+    try:
+        conn = psycopg2.connect(
+           host="localhost",
+            port="5432",
+            database="proyecto_semestral",
+            user="postgres",
+            password="."
+            )
+        return conn
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
+def run_query(query):
+    conn = connect_to_database()
+    if conn is not None:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            # Asumiendo que es una consulta SELECT, de lo contrario ajusta según sea necesario
+            rows = cur.fetchall()
+            return rows
+    else:
+        print("La conexión a la base de datos no se estableció.")
+        return None
+    
+def insert_data(query, data):
+    """
+    - query: Consulta SQL de inserción.
+    - data: Tupla con los datos a insertar.
+    """
+    conn = connect_to_database()
+    if conn is not None:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(query, data)
+                conn.commit()  # Confirma la transacción
+                print("Datos insertados correctamente.")
+        except Exception as e:
+            print(f"Error al insertar datos: {e}")
+        finally:
+            conn.close()  # Cierra la conexión
+    else:
+        print("La conexión a la base de datos no se estableció.")
---
+++ utils.py
@@ -0,0 +1,8 @@
+# Loading assets
+import requests
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
